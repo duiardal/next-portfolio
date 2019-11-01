e06c8bbd1d6d5f81cb57c543eeb33d24984e19dc
Dui Ardal <dui@Duis-MBP.lan>
2019-11-01 16:57:01 +0100

    changed the markup layout

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,16 +3,17 @@ import content from '../content/home.md';
 
 export default class Home extends Component {
   render() {
-    let { html , attributes:{ title, cats } } = content;
+    let { html , attributes:{ title, project } } = content;
     return (
       <article>
           <h1>{title}</h1>
           <div dangerouslySetInnerHTML={{ __html: html }}/>
           <ul>
-              { cats.map((cat, k) => (
+              { project.map((project, k) => (
                   <li key={k}>
-                    <h2>{cat.name}</h2>
-                    <p>{cat.description}</p>
+                    <h2>{project.name}</h2>
+                    <p>{project.description}</p>
+                    <img src={project.image.url}></img>
                   </li>
               ))}
           </ul>

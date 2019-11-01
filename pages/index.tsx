49c940e08760a4f7432c38c8e6719b0c3b002323
Dui Ardal <dui@Duis-MBP.lan>
2019-11-01 17:09:55 +0100

    test

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,19 +3,11 @@ import content from '../content/home.md';
 
 export default class Home extends Component {
   render() {
-    let { html , attributes:{ title, body } } = content;
+    let { html , attributes:{ title } } = content;
     return (
       <article>
           <h1>{title}</h1>
           <div dangerouslySetInnerHTML={{ __html: html }}/>
-          <ul>
-              { body.map((body, k) => (
-                  <li key={k}>
-                    <h2>{body.name}</h2>
-                    <p>{body.description}</p>
-                  </li>
-              ))}
-          </ul>
       </article>
     )
   }

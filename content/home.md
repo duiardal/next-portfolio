aaea481bbfbc0faf6f14d8c30e92c66a731d2106
Dui Ardal <dui@Duis-MBP.lan>
2019-11-01 17:02:18 +0100

    test

--- content/home.md
+++ content/home.md
@@ -1,6 +1,15 @@
 ---
 title: What's up
 date: 2019-03-17T19:31:20.591Z
+cats:
+  - description: Test cat
+    name: Maru (まる)
+  - description: Lil Bub is an American celebrity cat known for her unique appearance.
+    name: Lil Bub
+  - description: 'Grumpy cat is an American celebrity cat known for her grumpy appearance. '
+    name: Grumpy cat (Tardar Sauce)
+  - description: Lorem ipsum
+    name: TEST CAT YO
 ---
 Test **content**
 
--- pages/index.tsx
+++ pages/index.tsx
@@ -3,18 +3,16 @@ import content from '../content/home.md';
 
 export default class Home extends Component {
   render() {
-    let { html , attributes:{ title, project } } = content;
+    let { html , attributes:{ title, body } } = content;
     return (
       <article>
           <h1>{title}</h1>
           <div dangerouslySetInnerHTML={{ __html: html }}/>
           <ul>
-              { project.map((project, k) => (
+              { body.map((body, k) => (
                   <li key={k}>
-                    <h2>{project.name}</h2>
-                    <p>{project.description}</p>
-                    <img src={project.image.url}></img>
-                    <a href="https://focused-heyrovsky-c3a35e.netlify.com/static/admin">admin interface</a>
+                    <h2>{body.name}</h2>
+                    <p>{body.description}</p>
                   </li>
               ))}
           </ul>

1e26f9f859ee6cd9adaa39b3313e4e5bffd1d8f9
Dui Ardal <dui@Duis-MBP.lan>
2019-11-02 11:19:39 +0100

    test

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,29 @@
 import React, { Component } from 'react';
+import Link from 'next/link';
 
 import content from '../content/home.md';
 
 export default class Home extends Component {
-  render() {
-    const { attributes, html } = content;
-    return (
-      <React.Fragment>
-        <h1>{attributes.title}</h1>
-        <div dangerouslySetInnerHTML={{ __html: html }} />
-      </React.Fragment>
-    );
-  }
+    render() {
+        const { attributes, html } = content;
+        return (
+            <React.Fragment>
+                <h1>{attributes.title}</h1>
+                <React.Fragment>
+                    <nav>
+                        <Link href={'/'}>
+                            <a>home</a>
+                        </Link>
+                        <Link href={'/blog'}>
+                            <a>blog</a>
+                        </Link>
+                        <Link href={'/about'}>
+                            <a>about</a>
+                        </Link>
+                    </nav>
+                </React.Fragment>
+                <div dangerouslySetInnerHTML={{ __html: html }} />
+            </React.Fragment>
+        );
+    }
 }

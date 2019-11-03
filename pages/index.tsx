91a8b1674b94c776215e03efb1ffcdeac0531612
Dui Ardal <dui@Duis-MBP.lan>
2019-11-03 17:51:53 +0100

    fixed issues added styled components

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,27 +5,7 @@ import content from '../content/home.md';
 const Home = () => {
     const { attributes, html } = content;
     return (
-        <>
-            <h1>{attributes.title}</h1>
-            <div>
-                <li>
-                    <Link href={'/'}>
-                        <a>home</a>
-                    </Link>
-                </li>
-                <li>
-                    <Link href={'/projects'}>
-                        <a>projects</a>
-                    </Link>
-                </li>
-                <li>
-                    <Link href={'/about'}>
-                        <a>about</a>
-                    </Link>
-                </li>
-            </div>
-            <div dangerouslySetInnerHTML={{ __html: html }} />
-        </>
+        <div dangerouslySetInnerHTML={{ __html: html }} />
     );
 }
 
